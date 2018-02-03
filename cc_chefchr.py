5b8504488880cd75fab9ebeb30d50b9a96ed1066
suhalvemu <vemusuhal@gmail.com>
2018-02-03 13:44:54 +0530

    chef and his character feb2018codechef

--- cc_chefchr.py
+++ cc_chefchr.py
@@ -1,20 +1,27 @@
 from collections import Counter
-
-
-
-
+from itertools import permutations
 
 def islovely(lovely):
-    counter=Counter(lovely)
-    print(counter['c'],counter['h'],counter['e'],counter['f'])
+    count=0
+    perms=[''.join(p) for p in permutations('chef')]
+    for i in range(len(lovely)-3):
+        if lovely[i:i+4] in perms:
+            count = count + 1
+            
+    if count>0:
+        return 'lovely'+' '+str(count)
+    else:
+        return 'normal'
 
 testcase=int(input())
-
+list1=[]
 for i in range(testcase):
     string=input()
-    islovely(string)
+    res=islovely(string)
+    list1.append(res)
 
-
+for i in range(len(list1)):
+    print(list1[i])
 
 
 
